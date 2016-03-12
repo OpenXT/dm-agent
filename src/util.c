11bffc47fd3da05c9158a8fc4bbbf6129364dc8e
Eric Chanudet <chanudete@ainfosec.com>
2016-03-12 13:10:15 +0100

    s/_BSD_SOURCE/_DEFAULT_SOURCE/
    _DEFAULT_SOURCE should be used instead of _BSD_SOURCE since glibc 2.19:
    https://lwn.net/Articles/590381/
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- src/util.c
+++ src/util.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define _BSD_SOURCE
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
